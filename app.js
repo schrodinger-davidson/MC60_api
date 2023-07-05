16084a29e6644ff83f3cdbf2e813530e90778add
Sabir Iqbal <sabir@micelio.local>
2023-07-05 18:16:33 +0530

    added git ignore

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get("/",(req, res)=>{
+    res.send("Hello this is the test api");
+});
 
-    
-    
+const port =8000
+
+app.listen(port, ()=>{
+    console.log("A node js Api listening on port ")
 });
